ac1b14f5881a1f99a1876a8352e0a686a26f31dc
Jerome C <jcombes@sierrawireless.com>
2015-06-22 18:05:34 +0200

    README.md + mini refactoring

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # airbot
+========
+
+> Bash script and resources to set up a Hubot server on a blank Ubuntu AMI with a blank Volume attached.
+
+## Usage
+
+1. Install packages
+```bash
+./install.sh
+```
+2. Install hubot library
+```bash
+npm install
+```
+3. Modify the file *.airbotrc* and configure the Hubot API token
+4. Open a screen session
+```bash
+screen -S airbot
+```
+5. Launch Hubot server
+```bash
+./bin/airbot
+```
+6. Quit the screen session
+```bash
+ctrl a+d
+```
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Script to setup a Airbot server from a blank Ubuntu AMI with a blank attached volume
+
+echo "******** Install packages *********"
+
+sudo apt-get update -qqy
+sudo apt-get install -qqy --no-install-recommends \
+        curl \
+        git \
+        screen
+
+echo "******** Install nodejs and npm ********"
+export NODE_VERSION=0.12.4
+export NPM_VERSION=2.11.2
+curl -SLO "http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz"
+sudo tar -xzf "node-v$NODE_VERSION-linux-x64.tar.gz" -C /usr/local --strip-components=1
+sudo rm "node-v$NODE_VERSION-linux-x64.tar.gz"
+sudo npm install -g npm@"$NPM_VERSION"
