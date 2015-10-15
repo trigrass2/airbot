9343183877d9fc610ce271ead9b2679d7e4b38aa
David Sciamma <dsciamma@sierrawireless.com>
2015-10-15 22:10:01 +0200

    Add a webhook to add checklist to PR

--- scripts/github-autocomment-pr.js
+++ scripts/github-autocomment-pr.js
@@ -10,7 +10,7 @@ var token = process.env.GITHUB_TOKEN;
 
 var airbot;
 
-var genericChecklistComment = "Please write here the checklist to execute before merging this pull request. Here is an example:\n" +
+var defaultChecklistComment = "Please write here the checklist to execute before merging this pull request. Here is an example:\n" +
     "- [ ] Branch is up-to-date (master merged into it)\n" +
     "- [ ] Code review done\n" +
     "- [ ] Documentation updated\n" +
@@ -20,64 +20,55 @@ var genericChecklistComment = "Please write here the checklist to execute before
     "- [ ] Describe non-regression tests done\n" +
     "- [ ] Non-regression tests OK";
 
-var checklistComment = "Here is the checklist to complete before merging pull request:\n" +
-    "- [ ] Branch is up-to-date (master merged into it)\n" +
-    "- [ ] Code review done\n" +
-    "- [ ] Translation done (if required)\n" +
-    "- [ ] Non-regression tests updated\n" +
-    "- [ ] Documentation updated (API, N&N)\n" +
-    "- [ ] Performance tests done\n" +
-    "- [ ] Smoke tests reviewed\n" +
-    "- [ ] Feature validated\n" +
-    "- [ ] Smoke tests OK\n" +
-    "- [ ] Non-regression tests OK (put the list of non-regression tests done)\n" +
-    "- [ ] Jasmine tests written (if required)\n" +
-    "\n" +
-    "Don't hesitate to modify/complete this list. We just need to trace what was done and how.";
-
 var checklists = {};
+// Customize the checklist per GitHub repo here
+checklists["AirVantage/aws-lambda-sns-twilio"] = "My custom checklist:\n" +
+    "- [ ] Branch is up-to-date (master merged into it)\n" +
+    "- [ ] Describe the tests done\n";
 
 module.exports = function(robot) {
     airbot = robot;
 
-    airbot.router.post('/hubot/alert-twiml', function(req, res) {
+    airbot.router.post('/hubot/gh-autocomment-pr', function(req, res) {
         var payload = req.body;
-        var event = req.headers['x-github-event']
+        var event = req.headers['x-github-event'];
         if (event === "pull_request" && payload.pull_request && payload.action === "opened") {
-            console.log('Pull request opened');
-            if (payload.repository.full_name === "AirVantage/aws-lambda-sns-twilio") {
-                var github = new GitHubApi({
-                    // required
-                    version: "3.0.0",
-                    // optional
-                    debug: true,
-                    protocol: "https",
-                    host: "api.github.com",
-                    timeout: 5000,
-                    headers: {
-                        "user-agent": "airvantage-pullrequestagent-app" // GitHub is happy with a unique user agent
-                    }
-                });
-                github.authenticate({
-                    type: "oauth",
-                    token: token
-                });
+            var github = new GitHubApi({
+                // required
+                version: "3.0.0",
+                // optional
+                debug: true,
+                protocol: "https",
+                host: "api.github.com",
+                timeout: 5000,
+                headers: {
+                    "user-agent": "airvantage-pullrequestagent-app" // GitHub is happy with a unique user agent
+                }
+            });
+            github.authenticate({
+                type: "oauth",
+                token: token
+            });
 
-                github.issues.createComment({
-                        user: payload.repository.owner.login,
-                        repo: payload.repository.name,
-                        number: payload.pull_request.number,
-                        body: genericChecklistComment,
-                    },
-                    function(err, result) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            res.send("OK");
-                        }
-                    }
-                );
+            var comment = defaultChecklistComment;
+            if (event.repository.full_name in checklists) {
+                comment = checklists[event.repository.full_name];
             }
+            github.issues.createComment({
+                    user: payload.repository.owner.login,
+                    repo: payload.repository.name,
+                    number: payload.pull_request.number,
+                    body: comment,
+                },
+                function(err, result) {
+                    if (err) {
+                        console.log(err);
+                        res.send("NOK");
+                    } else {
+                        res.send("OK");
+                    }
+                }
+            );
         }
     });
 };
